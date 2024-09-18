ea061b0695067b060f0f15ddf0a1eecd3323b2ed
fadhil riyanto <me@fadev.org>
2024-09-18 15:04:21 +0700

    add func to sanitize emoji input

--- utilsfunc.py
+++ utilsfunc.py
@@ -1,6 +1,7 @@
 from time import time
 from dotenv import load_dotenv
 import os
+import emoji
 
 load_dotenv()
 
@@ -18,3 +19,34 @@ def get_file_id(msg):
                 return msg.reply_to_message.animation.file_id
         if msg.reply_to_message.sticker != None:
                 return msg.reply_to_message.sticker.file_id
+        
+def sanitize_emoji(msg):
+    
+
+    try:
+        cur = msg.command[1]
+        
+        if len(msg.command[1]) > 1:
+            return {
+                "err": 1,
+                "msg": "A custom emoji must be just 1 chars length",
+                "ret": None
+            }
+        
+        if emoji.is_emoji(msg.command[1]) == False:
+            return {
+                "err": 1,
+                "msg": "You need emoji, not a chars",
+                "ret": None
+            }
+    except IndexError:
+        cur = "🗿"
+        
+    return {
+        "err": 0,
+        "msg": None,
+        "ret": cur
+    }
+            
+
+                        
